07433b954cfa866a6e29a48b4f4d695ca361e0cb
Martim Mas e Braga <mmeb1@st-andrews.ac.uk>
2024-12-17 13:59:11 -0300

    corrected shelf oscillation orientation and time axes missing last time step

--- nc_examples/create_forcings_examples.py
+++ nc_examples/create_forcings_examples.py
@@ -8,10 +8,10 @@ Created on Sun Dec 15 17:45:29 2024
 import numpy  as np
 import pandas as pd
 import xarray as xr
-import matplotlib.pyplot as plt
-import datetime as dt
+#import matplotlib.pyplot as plt
+#import datetime as dt
 
-example_number = 1
+example_number = 3
 
 # depth vector for shelf forcing
 zs = np.linspace(0,800,40)
@@ -21,6 +21,7 @@ zs = np.linspace(0,800,40)
 time_begin = pd.to_datetime('2010-01-01')
 t_begin    = 0.0
 dt_model   = 0.2
+
 
 match example_number:
     case 1:
@@ -34,11 +35,13 @@ match example_number:
         
         # Subglacial discharge forcing
         tsg = np.arange(t_begin,t_end,dt_model)     # create time axis
+        tsg = np.concatenate((tsg,[tsg[-1]+dt_model]),axis=0)
         qsg = 300*np.exp(-((tsg % 365-200)/30)**2); # subglacial discharge on tsg (idealised seasonal gaussian peaked at julian day 200)
     case 2: 
         # time axis
         t_end = 100
-        ts = np.arange(t_begin,t_end,dt_model) 
+        ts = np.arange(t_begin,t_end,dt_model)
+        ts = np.concatenate((ts,[ts[-1]+dt_model]),axis=0) # we need to append the last time step not included by np.arange
 
         # shelf forcing
         Sbottom = 35 # salinity at bottom
@@ -47,9 +50,11 @@ match example_number:
         Ttop    = 0  # temperature at top
         tw      = 10 # period of oscillation (days)
         zi      = 50+(30/2)*np.sin(2*np.pi*ts/tw) # 'pycnocline' oscillation
+        t_shelf = np.zeros([len(zs),len(ts)])
+        s_shelf = np.zeros([len(zs),len(ts)])
         for k in range(len(ts)):
-            t_shelf[:,k] = Tbottom-(Tbottom-Ttop)*np.exp(zs/zi[k]); 
-            s_shelf[:,k] = Sbottom-(Sbottom-Stop)*np.exp(zs/zi[k]); 
+            t_shelf[:,k] = np.flip(Tbottom-(Tbottom-Ttop)*np.exp(-np.flip(zs/zi[k]))); 
+            s_shelf[:,k] = np.flip(Sbottom-(Sbottom-Stop)*np.exp(-np.flip(zs/zi[k]))); 
 
         # in this example there is no subglacial discharge
         tsg = ts
@@ -58,6 +63,7 @@ match example_number:
         # time axis
         t_end = 100
         ts = np.arange(t_begin,t_end,dt_model) # time vector for shelf forcing
+        ts = np.concatenate((ts,[ts[-1]+dt_model]),axis=0) # we need to append the last time step not included by np.arange
         
         # shelf forcing
         t_shelf = 3*np.ones([len(zs),len(ts)])
@@ -69,7 +75,8 @@ match example_number:
         # time axis
         t_end   = 2*365
         ts      = np.arange(t_begin,t_end,dt_model) # time vector for shelf forcing
-
+        ts      = np.concatenate((ts,[ts[-1]+dt_model]),axis=0)
+        
         # shelf forcing
         Sbottom = 35 # salinity at bottom
         Stop    = 30 # salinity at top
@@ -77,14 +84,16 @@ match example_number:
         Ttop    = 0 #temperature at top
         tw      = 10 # period of oscillation (days)
         zi      = 50+(30/2)*np.sin(2*np.pi*ts/tw) # 'pycnocline' oscillation
+        t_shelf = np.zeros([len(zs),len(ts)])
+        s_shelf = np.zeros([len(zs),len(ts)])
         for k in range(len(ts)):
-            s_shelf[:,k] = Sbottom-(Sbottom-Stop)*np.exp(zs/zi[k]); # shelf salinity
-            t_shelf[:,k] = Tbottom-(Tbottom-Ttop)*np.exp(zs/zi[k]); # shelf temperature
+            t_shelf[:,k] = np.flip(Tbottom-(Tbottom-Ttop)*np.exp(-np.flip(zs/zi[k]))); 
+            s_shelf[:,k] = np.flip(Sbottom-(Sbottom-Stop)*np.exp(-np.flip(zs/zi[k]))); 
             
         # glacier forcing
-        tsg = np.arange(t_begin,t_end,dt_model) # create time axis
+        tsg = ts
         qsg = 300*np.exp(-((tsg % 365-200)/30)**2); # subglacial discharge on tsg
-
+        # the icebergs are dealt with in the config file
 
 
 n_plumes = 1 # choose number of plumes
@@ -95,19 +104,23 @@ gl_depth = 800.0
 gl_depth_all_plumes = np.ones(n_plumes)*gl_depth                   # replicate the same GL for all plumes
 qsg_all_plumes      = np.repeat(qsg[np.newaxis,:],n_plumes,axis=0) # replicate Qsg for all plumes
 
-#TODO: convert time axes (ts & tsg) to the proper units: starting at 2010-01-01
+# convert time axes (ts & tsg) to the proper units: starting at `time_begin`
+# ts has a time resolution of "days"
+time_end = time_begin + pd.Timedelta(days=t_end)
+tsg_datetime = pd.date_range(start=time_begin,end=time_end,freq='0.2D')
+ts_datetime = pd.date_range(start=time_begin, end=time_end,periods=len(ts))
 
 # set up output datasets
-ds_shelf = xr.Dataset(coords=dict(depth=(['depth'],zs),time=(['time'],ts)), \
+ds_shelf = xr.Dataset(coords=dict(depth=(['depth'],zs),time=(['time'],ts_datetime)), \
                       attrs=dict(run_name=f"example{example_number}_subglacial_discharge"))
     
-ds_glacier = xr.Dataset(coords=dict(plume=(['plume'],i_plumes),time=(['time'],tsg)), \
+ds_glacier = xr.Dataset(coords=dict(plume=(['plume'],i_plumes),time=(['time'],tsg_datetime)), \
                       attrs=dict(run_name=f"example{example_number}_subglacial_discharge"))
     
     
-ds_Ts  = xr.DataArray(t_shelf,coords={'depth':zs,'time':ts})
-ds_Ss  = xr.DataArray(s_shelf,coords={'depth':zs,'time':ts})
-ds_Qsg = xr.DataArray(qsg_all_plumes,coords={'plume':i_plumes,'time':tsg})
+ds_Ts  = xr.DataArray(t_shelf,coords={'depth':zs,'time':ts_datetime})
+ds_Ss  = xr.DataArray(s_shelf,coords={'depth':zs,'time':ts_datetime})
+ds_Qsg = xr.DataArray(qsg_all_plumes,coords={'plume':i_plumes,'time':tsg_datetime})
 ds_Hgl = xr.DataArray(gl_depth_all_plumes,coords={'plume':i_plumes})
 
 ds_shelf['thetao'] = ds_Ts
@@ -116,5 +129,5 @@ ds_glacier['Qsg'] = ds_Qsg
 ds_glacier['Hgl'] = ds_Hgl
 
 # saving...
-ds_shelf.to_netcdf(f"example{example_number}_shelf_forcings.nc")
-ds_glacier.to_netcdf(f"example{example_number}_glacier_forcings_np{n_plumes}+'.nc")
+ds_shelf.to_netcdf(f"example{example_number}_shelf_forcings.nc",mode='w')
+ds_glacier.to_netcdf(f"example{example_number}_glacier_forcings_np{n_plumes}.nc",mode='w')
