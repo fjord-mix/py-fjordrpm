5b516ca1664bf2bda9ce5374ff35fcf2c0e81855
Martim Mas e Braga <mmeb1@st-andrews.ac.uk>
2024-12-20 09:03:20 -0300

    still not properly implemented, just a quick check

--- fjordrpm_plotting.py
+++ fjordrpm_plotting.py
@@ -7,8 +7,8 @@ Created on Mon Dec 16 18:44:07 2024
 """
 import xarray as xr
 
-path_in = '/Users/mmeb1/fjordrpm_coupling/test_inputs/example2_intermediary/'
-file_in = 'outputs_example2_intermediary.nc'
+path_in = '/Users/mmeb1/fjordrpm_coupling/outputs/example_restart_from3/'
+file_in = 'outputs_example_restart_from3.nc'
 
 
 ds = xr.open_dataset(path_in+file_in)
