4de4c6326a0e18b96309ca2c9394c37ba3bdc582
Martim Mas e Braga <mmeb1@st-andrews.ac.uk>
2024-12-20 09:02:55 -0300

    left out code-profiling option as a comment

--- run_fjordrpm.py
+++ run_fjordrpm.py
@@ -22,5 +22,17 @@ from fjordrpm import FjordRPM
 
 #fjord_run = Feshie(sys.argv[1])
 #fjord_run = FjordRPM('examples/config_example_KangTest.toml')
-fjord_run = FjordRPM('examples/config_example4.toml')
+#fjord_run = FjordRPM('examples/config_example_restart3.toml')
+fjord_run = FjordRPM('examples/config_example2.toml')
 fjord_run.run_fjord()
+
+# Use the block below if profiling the code
+# =============================================================================
+#import cProfile
+#import pstats
+# cProfile.run('fjord_run.run_fjord()','./test_inputs/profiling_example4_opt2')
+# p = pstats.Stats('./test_inputs/profiling_example4_opt2')
+# p.strip_dirs()
+# p.sort_stats(pstats.SortKey.CUMULATIVE)
+# p.print_stats(10)
+# =============================================================================
